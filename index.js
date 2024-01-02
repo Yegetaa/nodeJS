941b71aacf5d3f9488acd970ce1bed59cf783f20
Yegeta <149103185+Yegetaa@users.noreply.github.com>
2024-01-02 14:54:38 -0500

    "different routes added"

--- index.js
+++ index.js
@@ -4,10 +4,29 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Hello World!\n");
-})
+    res.statusCode = 200;  
+    if (req.url === '/'){
+        res.status = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.write("<h1>Hello Node</h1>");
+        res.end();
+    } else if (req.url === '/test') {
+        res.status = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.write("<h2>This is a test</h2>");
+        res.end();
+    } else if (req.url === '/products') {
+        res.status = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.write("<h3>Products Page</h3>");
+        res.end();
+    } else {
+        res.status = 404;
+        res.setHeader("Content-Type", "text/html");
+        res.write("<h1>Page Not Found!</h1>");
+        res.end();
+    }
+});
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
